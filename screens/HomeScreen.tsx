190e9e99bf582958bc8e2ea4cbf97e463df2eb90
Kene Aniekwena <kc.boys@live.com>
2021-06-21 13:33:10 +0100

    Wip

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,6 @@
-import React, { useContext, useEffect } from "react";
-import { Button, Icon, Layout, Text, useTheme } from "@ui-kitten/components";
-import { View, TouchableOpacity, Image, ScrollView } from "react-native";
+import React, { useContext } from "react";
+import { Layout, Text, useTheme } from "@ui-kitten/components";
+import { View, ScrollView } from "react-native";
 import FullColor from "./components/headers/FullColor";
 import FullImage from "./components/headers/FullImage";
 import HalfImage from "./components/headers/HalfImage";
@@ -8,17 +8,17 @@ import SplitColor from "./components/headers/SplitColor";
 import renderSwitch from "../hooks/renderSwitch";
 import ListByTable from "./components/currency-list/ListByTable";
 import ListByCards from "./components/currency-list/ListByCards";
-import useScreenSize from "../hooks/useScreenSize";
 import SingleNav from "./components/navbars/SingleNav";
 import SplitNav from "./components/navbars/SplitNav";
 import { useNavigation } from "@react-navigation/core";
 import { MainApiContext } from "../contexts/ApiContexts";
 
 export default function HomeScreen() {
-  const navigation = useNavigation();
   const theme = useTheme();
 
   const { settings } = useContext(MainApiContext);
+
+  console.log({ settings });
 
   return (
     <Layout style={{ flex: 1 }}>
